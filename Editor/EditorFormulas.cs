e39c2e953ba84b0f9ed86708e538b9e618e4edd9
Yilmaz Kiymaz <ylmzkymz@gmail.com>
2016-06-29 18:40:28 +0300

    Initial commit.

---
+++ Editor/EditorFormulas.cs
@@ -0,0 +1,269 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+using System.Collections.Generic;
+
+public static class EditorFormulas {
+
+	public static void LogActiveEditorWindow()
+	{
+		Debug.Log(string.Format("{0} of type {1}", EditorWindow.focusedWindow.titleContent.text, EditorWindow.focusedWindow.GetType().FullName));
+	}
+
+	public static void ShowAllAssemblies()
+	{
+		var assemblies = System.AppDomain.CurrentDomain.GetAssemblies();
+		foreach(var assembly in assemblies) {
+			Debug.Log("Assembly: " + assembly.FullName);
+		}
+	}
+
+	public static void SaveEditorGUISkinToAssetDatabase()
+	{
+		var currentGUISkin = GetEditorGUISkin();
+
+		var newGUISkin = Object.Instantiate(currentGUISkin);
+		newGUISkin.hideFlags = HideFlags.None;
+
+		var assetPath = AssetDatabase.GenerateUniqueAssetPath("Assets/EditorGUISkin.guiskin");
+		CreateAndSaveAsset(newGUISkin, assetPath);
+	}
+
+	private static GUISkin GetEditorGUISkin()
+	{
+		return typeof(GUISkin).GetField("current", ReflectionHelper.fullBindingStatic).GetValue(null) as GUISkin;
+	}
+
+	public static void SelectEditorGUISkin()
+	{
+		Selection.activeObject = GetEditorGUISkin();
+	}
+
+	public static void ReportAssetPathOfSelectedObject()
+	{
+		Debug.Log(AssetDatabase.GetAssetPath (Selection.activeObject));
+	}
+
+	public static void CopyEditorStyleToSelectedGUISkin(string styleName)
+	{
+		var selectedGUISkin = Selection.activeObject as GUISkin;
+		if(selectedGUISkin == null)
+		{
+			Debug.Log("Selected object is not a GUISkin");
+			return;
+		}
+
+		var editorGUISkin = GetEditorGUISkin();
+		var style = editorGUISkin.FindStyle(styleName);
+		if(style == null)
+		{
+			Debug.Log(string.Format("No style with name {0} found in editor GUI skin", styleName));
+			return;
+		}
+
+		var existingStyle = selectedGUISkin.FindStyle(styleName);
+		if(existingStyle != null)
+		{
+			Debug.Log(string.Format("Selected gui skin already contains a style with name {0}", styleName));
+			return;
+		}
+
+		var listOfStyles = new List<GUIStyle>(selectedGUISkin.customStyles);
+		var styleInstance = new GUIStyle(style);
+		listOfStyles.Add(styleInstance);
+		selectedGUISkin.customStyles = listOfStyles.ToArray();
+
+		DirtyAndSaveAsset(selectedGUISkin);
+	}
+
+	public static void ClearStyleCacheOfSelectedGUISkin()
+	{
+		var selectedGUISkin = Selection.activeObject as GUISkin;
+		if(selectedGUISkin == null)
+		{
+			Debug.Log("Selected object is not a GUISkin");
+			return;
+		}
+
+		typeof(GUISkin).GetField("m_Styles", ReflectionHelper.fullBinding).SetValue(selectedGUISkin, null);
+	}
+
+	public static void SetBorderOfStylesStartingWithNameInSelectedGUISkin(string styleNameStartsWith, RectOffset border)
+	{
+		var skin = Selection.activeObject as GUISkin;
+		DoActionOnStylesStartingWithNameInGUISkin(skin, styleNameStartsWith, SetBorder, border);
+		DirtyAndSaveAsset(skin);
+	}
+
+	public static void SetMarginOfStylesStartingWithNameInSelectedGUISkin(string styleNameStartsWith, RectOffset margin)
+	{
+		var skin = Selection.activeObject as GUISkin;
+		DoActionOnStylesStartingWithNameInGUISkin(skin, styleNameStartsWith, SetMargin, margin);
+		DirtyAndSaveAsset(skin);
+	}
+
+	public static void SetPaddingOfStylesStartingWithNameInSelectedGUISkin(string styleNameStartsWith, RectOffset padding)
+	{
+		var skin = Selection.activeObject as GUISkin;
+		DoActionOnStylesStartingWithNameInGUISkin(skin, styleNameStartsWith, SetPadding, padding);
+		DirtyAndSaveAsset(skin);
+	}
+
+	public static void SetOverflowOfStylesStartingWithNameInSelectedGUISkin(string styleNameStartsWith, RectOffset overflow)
+	{
+		var skin = Selection.activeObject as GUISkin;
+		DoActionOnStylesStartingWithNameInGUISkin(skin, styleNameStartsWith, SetOverflow, overflow);
+		DirtyAndSaveAsset(skin);
+	}
+
+	public static void SetContentOffsetOfStylesStartingWithNameInSelectedGUISkin(string styleNameStartsWith, Vector2 contentOffset)
+	{
+		var skin = Selection.activeObject as GUISkin;
+		DoActionOnStylesStartingWithNameInGUISkin(skin, styleNameStartsWith, SetContentOffset, contentOffset);
+		DirtyAndSaveAsset(skin);
+	}
+
+	public static void TestFindAsset()
+	{
+		var asset = Selection.activeObject;
+		if(asset = null) { return; }
+		if(!AssetDatabase.Contains(asset))
+		{
+			return;
+		}
+		var searchFilterType = ReflectionHelper.GetTypeInfo("SearchFilter");
+		var searchFilterConstructor = searchFilterType.type.GetConstructor(System.Type.EmptyTypes);
+		var searchFilter = searchFilterConstructor.Invoke(null);
+		//Search all assets
+		searchFilterType.GetPropertyInfo("searchArea").SetValue(searchFilter, 0, null);
+		//Set referencing instance ids to guid of material asset
+		searchFilterType.GetPropertyInfo("referencingInstanceIDs").SetValue(searchFilter, new [] {asset.GetInstanceID()}, null);
+		var assetDatabaseType = typeof(AssetDatabase);
+		var findAssetsMethod = assetDatabaseType.GetMethod("FindAssets", ReflectionHelper.fullBinding, null, new [] {searchFilterType.type}, null);
+		var guids = (string[]) findAssetsMethod.Invoke(null, new System.Object[] {searchFilter});
+		foreach(var guid in guids)
+		{
+			Debug.Log(AssetDatabase.GUIDToAssetPath(guid));
+		}
+	}
+
+	public static void SceneObjectInstanceIDTest()
+	{
+		var objs = UnityEditorInternal.InternalEditorUtility.LoadSerializedFileAndForget("Assets/testScene1.unity");
+		for(int i=0; i<objs.Length; i++)
+		{
+			var obj = objs[i];
+			if(obj is GameObject && obj.name == "Sphere")
+			{
+				Debug.Log(obj.GetInstanceID());
+				// destroy the scene Objects
+				Object.DestroyImmediate(obj);
+				System.GC.Collect();
+				Resources.UnloadUnusedAssets();
+			}
+		}
+
+		objs = UnityEditorInternal.InternalEditorUtility.LoadSerializedFileAndForget("Assets/testScene1.unity");
+		for(int i=0; i<objs.Length; i++)
+		{
+			var obj = objs[i];
+			if(obj is GameObject && obj.name == "Sphere")
+			{
+				Debug.Log(obj.GetInstanceID());
+				// destroy the scene Objects
+				Object.DestroyImmediate(obj);
+				System.GC.Collect();
+				Resources.UnloadUnusedAssets();
+			}
+		}
+	}
+
+	public static void ReportInstanceID()
+	{
+		if(Selection.activeObject != null)
+		{
+			Debug.Log(Selection.activeObject.GetInstanceID());
+		}
+	}
+
+	public static void PrintOutMethodBody()
+	{
+
+	}
+
+	private static void SetBorder(GUIStyle style, object parameter)
+	{
+		style.border = (RectOffset) parameter;
+	}
+
+	private static void SetMargin(GUIStyle style, object parameter)
+	{
+		style.margin = (RectOffset) parameter;
+	}
+
+	private static void SetPadding(GUIStyle style, object parameter)
+	{
+		style.padding = (RectOffset) parameter;
+	}
+
+	private static void SetOverflow(GUIStyle style, object parameter)
+	{
+		style.overflow = (RectOffset) parameter;
+	}
+
+	private static void SetContentOffset(GUIStyle style, object parameter)
+	{
+		style.contentOffset = (Vector2) parameter;
+	}
+
+	private static void DoActionOnStylesStartingWithNameInGUISkin(GUISkin skin, string styleNameStartsWith, System.Action<GUIStyle, object> action, object parameter)
+	{
+		if(skin == null) { return; }
+		foreach (var style in skin.customStyles) {
+			if (!style.name.StartsWith (styleNameStartsWith)) {
+				continue;
+			}
+			action (style, parameter);
+		}
+	}
+
+
+	private static void DirtyAndSaveAsset(Object obj)
+	{
+		if(obj == null) { return; }
+		EditorUtility.SetDirty(obj);
+		AssetDatabase.SaveAssets();
+		AssetDatabase.Refresh();
+	}
+
+	private static void CreateAndSaveAsset(Object obj, string path)
+	{
+		if(obj == null) { return; }
+		AssetDatabase.CreateAsset (obj, path);
+		AssetDatabase.SaveAssets ();
+		AssetDatabase.Refresh();
+	}
+
+	private static void CreateAsset<T> () where T : ScriptableObject
+	{
+		T asset = ScriptableObject.CreateInstance<T> ();
+
+		string path = AssetDatabase.GetAssetPath (Selection.activeObject);
+		if (path == "" || !path.StartsWith("Assets"))
+		{
+			path = "Assets";
+		}
+		else if (Path.GetExtension (path) != "") 
+		{
+			path = path.Replace (Path.GetFileName (AssetDatabase.GetAssetPath (Selection.activeObject)), "");
+		}
+
+		string assetPathAndName = AssetDatabase.GenerateUniqueAssetPath (path + "/New " + typeof(T).ToString() + ".asset");
+
+		Debug.Log(assetPathAndName);
+		CreateAndSaveAsset(asset, assetPathAndName);
+		EditorUtility.FocusProjectWindow ();
+		Selection.activeObject = asset;
+	}
+
+}
---
+++ Editor/EditorFormulasWindow.cs
@@ -0,0 +1,155 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Linq;
+
+public class EditorFormulasWindow : EditorWindow {
+
+	MethodInfo[] formulaMethods;
+	Dictionary<MethodInfo, object[]> parameterValuesDictionary;
+	Dictionary<MethodInfo, ParameterInfo[]> parametersDictionary;
+	Dictionary<MethodInfo, bool> toggleStateDictionary;
+
+	public GUIStyle foldout;
+	private bool initStyles = false;
+
+	private Vector2 scrollPos;
+
+	[MenuItem ("Window/Editor Formulas %#e")]
+	public static void DoWindow()
+	{
+		EditorWindow.GetWindow<EditorFormulasWindow>();
+	}
+
+	void OnEnable()
+	{
+		Refresh();
+	}
+
+	void Refresh()
+	{
+		formulaMethods = ReflectionHelper.GetTypeInfo("EditorFormulas", "Assembly-CSharp-Editor").type.GetMethods(BindingFlags.Static | BindingFlags.Public);
+		parametersDictionary = new Dictionary<MethodInfo, ParameterInfo[]>(formulaMethods.Length);
+		parameterValuesDictionary = new Dictionary<MethodInfo, object[]>(formulaMethods.Length);
+		toggleStateDictionary = new Dictionary<MethodInfo, bool>(formulaMethods.Length);
+		foreach(var method in formulaMethods)
+		{
+			parametersDictionary.Add(method, method.GetParameters());
+			parameterValuesDictionary.Add(method, new object[method.GetParameters().Length]);
+			toggleStateDictionary.Add(method, false);
+		}
+	}
+
+	void OnGUI()
+	{
+		if(!initStyles)
+		{
+			initStyles = true;
+		}
+
+		scrollPos = EditorGUILayout.BeginScrollView(scrollPos);
+
+		for(int i=0; i<formulaMethods.Length; i++)
+		{
+			var method = formulaMethods[i];
+			if(method == null) { continue; }
+
+			var parameters = parametersDictionary[method];
+			var parameterValuesArray = parameterValuesDictionary[method];
+
+			var niceName = ObjectNames.NicifyVariableName(method.Name);
+
+			GUI.enabled = parameters.Length == 0 || parameterValuesArray.All(x => x != null);
+			if(GUILayout.Button(niceName))
+			{
+				method.Invoke(null, parameterValuesArray);
+			}
+			GUI.enabled = true;
+
+			if(parameters.Length > 0)
+			{
+				GUILayout.BeginHorizontal();
+				GUILayout.Space(15f);
+				GUILayout.BeginVertical();
+				//Draw parameter fields
+				for (int p=0; p<parameters.Length; p++) {
+					var parameter = parameters[p];
+					var parameterType = parameter.ParameterType;
+					var valueObj = parameterValuesArray[p];
+					GUILayout.BeginHorizontal();
+					GUILayout.Label(new GUIContent(parameter.Name, parameter.Name));//, GUILayout.Width(100));
+					object newValue = null;
+
+	//				if(parameterType == typeof(Object))
+	//				{
+	//					newValue = EditorGUILayout.ObjectField(valueObj != null ? ((Object) valueObj) : null, parameterType, true);
+	//				}
+	//				else if(parameterType.IsClass && parameterType.IsSerializable)
+	//				{
+	//					var fieldInfos = parameterType.GetFields(BindingFlags.Instance | BindingFlags.Public);
+	//					//TODO: Draw a field for each public instance field of class
+	//				}
+
+					EditorGUI.BeginChangeCheck();
+					if (parameterType == typeof(int)) {
+						newValue = EditorGUILayout.IntField(valueObj != null ? ((int) valueObj) : 0 );
+					}
+					else if(parameterType == typeof(float))
+					{
+						newValue = EditorGUILayout.FloatField(valueObj != null ? ((float) valueObj) : 0f );
+					}
+					else if(parameterType == typeof(string))
+					{
+						newValue = EditorGUILayout.TextField(valueObj != null ? ((string) valueObj) : string.Empty );
+					}
+					else if(parameterType == typeof(Rect))
+					{
+						newValue = EditorGUILayout.RectField(valueObj != null ? ((Rect) valueObj) : new Rect() );
+					}
+					//TODO: Don't do this, instead use RectOffset as a class
+					else if(parameterType == typeof(RectOffset))
+					{
+						//We use a Vector4Field for RectOffset type because there isn't 
+						var rectOffset = (RectOffset) valueObj;
+						var vec4 = EditorGUILayout.Vector4Field(string.Empty, valueObj != null ? new Vector4(rectOffset.left, rectOffset.right, rectOffset.top, rectOffset.bottom) : Vector4.zero );
+						newValue = new RectOffset((int)vec4.x, (int)vec4.y, (int)vec4.z, (int)vec4.w);
+					}
+					else if(parameterType == typeof(Vector2))
+					{
+						newValue = EditorGUILayout.Vector2Field(string.Empty, valueObj != null ? ((Vector2) valueObj) : Vector2.zero );
+					}
+					else if(parameterType == typeof(Vector3))
+					{
+						newValue = EditorGUILayout.Vector3Field(string.Empty, valueObj != null ? ((Vector3) valueObj) : Vector3.zero );
+					}
+					else if(parameterType == typeof(Vector4))
+					{
+						newValue = EditorGUILayout.Vector4Field(string.Empty, valueObj != null ? ((Vector4) valueObj) : Vector4.zero );
+					}
+					else if(parameterType == typeof(Color))
+					{
+						newValue = EditorGUILayout.ColorField(valueObj != null ? ((Color) valueObj) : Color.white);
+					}
+					else if(parameterType == typeof(Object))
+					{
+						newValue = EditorGUILayout.ObjectField(valueObj != null ? ((Object) valueObj) : null, parameterType, true);
+					}
+					else if(parameterType.IsEnum)
+					{
+						newValue = EditorGUILayout.EnumPopup(valueObj != null ? ((System.Enum)valueObj) : default(System.Enum));
+					}
+					if(EditorGUI.EndChangeCheck())
+					{
+						parameterValuesArray[p] = newValue;
+					}
+					GUILayout.EndHorizontal();
+				}
+				GUILayout.EndVertical();
+				GUILayout.EndHorizontal();
+			}
+		}
+
+		EditorGUILayout.EndScrollView();
+	}
+}
